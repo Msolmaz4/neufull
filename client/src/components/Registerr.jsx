c8170becd305f8dc09267b5103a4307dffd3faff
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-09-01 22:52:32 +0200

    registeraxios

--- client/src/components/Registerr.jsx
+++ client/src/components/Registerr.jsx
@@ -1,95 +1,92 @@
-import React,{useRef} from "react";
+import React, { useRef } from "react";
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
 
 const Registerr = () => {
+ //burada userContexden caguirdik 
+ //form on submitte biy register gondeririy yaten oda obje
+ //onSubmit={()=>register(inputRef.curent)}
+  const {register} = useContext(UserContext)
 
 
-  const nameRef =useRef()
+
+
+  /**
+ *  const nameRef =useRef()
   const surnameRef =useRef()
   const emailRef =useRef()
   const passwordRef=useRef()
+ */
 
+  // burada input ref icine atariz
+  const inputRef = useRef({
+    name: "",
+    surname: "",
+    email: "",
+    password: ""
+    
+    
+  });
 
+  //biy burad degisikliklerde name ve value aliriy bunuda const atariy kolay olsun diye
+  //burada dikkat etmek gereken namae ler onmeli
 
-
-
-
-
-
-
-
-
-
-
- // burada input ref icine atariz
-  /** const inputRef =useRef({
-        email:'',
-        password:'',
-        name:'',
-        surname:''
-    })
-   *  */ 
- //biy burad degisikliklerde name ve value aliriy bunuda const atariy kolay olsun diye
- //burada dikkat etmek gereken namae ler onmeli 
-
- const handle =(e)=>{
-e.preventDefault()
-    console.log(
+  const handle = (e) => {
+    /** burda genelde kullanilan ama
+ * ref vermeyi unutmq
+ *  console.log(
         nameRef.current.value,
         surnameRef.current.value,
         emailRef.current.value,
         passwordRef.current.value
         )
-
-   
-   /** const  name =e.target.value
-    const value =e.target.value
+ */
+//burada nameyi dikkat et cunku burda input taki nameleri alizoruz
+    const name = e.target.name;
+    const value = e.target.value;
     //inputef alifimiyda current gelir
     //curret yaptigim degisiklik burada name uyerinden cekeriz
-    inputRef.current[name]= value
+    inputRef.current[name] = value;
     //consol flase true cevir isAuthen
     //console.log(inputRef.current)
-    *  */
-      
-    
-
-
- }
-  
+    //console.log(inputRef.current);
+  };
 
   return (
     <div>
       <div className="register">
         <div className="sol"></div>
         <div className="sag">
-          <form onSubmit={handle}>
+          <form onSubmit={(e)=>register(e,inputRef.current)} >
             <div className="formm">
               <div>
                 <label>Name</label>
                 <br />
-                <input type="text"  ref={nameRef} onChange={handle}/>
+                <input type="text" /*ref={nameRef}*/ name="name" onChange={handle}/>
               </div>
               <div>
                 <label htmlFor=""> surname</label>
                 <br />
-                <input type="text" ref={surnameRef} onChange={handle}/>
+                <input type="text" name="surname" onChange={handle} />
               </div>
               <div>
                 <label htmlFor="">email</label>
                 <br />
-                <input type="text"  ref={emailRef} onChange={handle}/>
+                <input type="text" name="email" onChange={handle} />
               </div>
               <div>
                 <label htmlFor="">Password</label>
                 <br />
-                <input type="text" ref={passwordRef} onChange={handle}/>
+                <input type="text" name="password" onChange={handle} />
                 <br />
                 <br />
               </div>
-             
+              <div className="ddd">
+                <button type="submit" onSubmit={(e)=>register(e,inputRef.current)}>
+                  Signup
+                </button>
+              </div>
             </div>
-            <div className="ddd">
-                <button type='submit' onC={handle}>Signup</button>
-              </div>
           </form>
         </div>
       </div>
@@ -98,4 +95,3 @@ e.preventDefault()
 };
 
 export default Registerr;
-  
--- client/src/context/UserContext.js
+++ client/src/context/UserContext.js
@@ -1,8 +1,12 @@
 // burda once temel yaoyi kurariy
 //1
 import { createContext, useEffect, useState } from "react";
+
+import axios from 'axios'
 //1
 export const UserContext = createContext()
+
+
 
 //1
 export const UserProvider = ({children})=>{
@@ -14,9 +18,15 @@ export const UserProvider = ({children})=>{
     //2 bunu temel olarka yaz sonra useeffrct kur tokenui cagir
     //tokenle gonderirirz cunku backende kontrol yapmak icin
     const checkAuth = (token)=>{
-
+   
+      
 
     }
+
+
+
+
+
   //3  token && checkAuth(token) flase gleirse kontrol yapilmaz
   useEffect(()=>{
     
@@ -26,11 +36,21 @@ export const UserProvider = ({children})=>{
 
   },[])
 
+//4 registeri kurduk simdi bunu axios indirdik sonra axuiosla baglanti kuracagiy backend
+      //burada backende register kayit uyantisina dikkat et yoksa hata verir
+      //burad obje gonderriy bunuda regisdter gonderriy
 
+      const register = (e,user)=>{
+        //e yapariy sayfa yenilenmesin ,comsoldan korol yapariy gelip geledigini
+        console.log('register',user)
+        e.preventDefault()
+        axios.post('http://localhost:8006/user/register',user)
+
+      }
 
 //1
     return(
-        <UserContext.Provider value={{ isAuthenticated }}>
+        <UserContext.Provider value={{ isAuthenticated ,register}}>
             {children}
         </UserContext.Provider>
     )
